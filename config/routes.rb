8a02cb71636db94e70416e511257b2bbd8e71c93
pchabbi7 <pri.17feb@gmail.com>
2018-07-18 00:53:07 +0530

    Routing Details

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+namespace :admin do 
+  get 'dashboard/main'
+  get 'dashboard/user'
+  get 'dashboard/blog'
+end 	
+
+
+  get 'about' , to: 'pages#about'
+  get 'legend/abc/lead' , to: 'pages#contact' , as: 'lead'
+
+  resources :blogs
+  resources :posts
+
+  get 'posts/*missing' , to: 'posts#missing'
+   
+  get 'pages/:else' , to: 'pages#something' 
+
+  root to: 'pages#home'
+end
