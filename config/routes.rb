883a123af27ed2a5bd2691c514c30f8938c3546b
pchabbi7 <pri.17feb@gmail.com>
2018-07-18 00:57:12 +0530

    Routes file modified

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ end
 
   get 'posts/*missing' , to: 'posts#missing'
    
-  get 'pages/:else' , to: 'pages#something' 
+  get 'pages/:else/:another_one' , to: 'pages#something' 
 
   root to: 'pages#home'
 end
